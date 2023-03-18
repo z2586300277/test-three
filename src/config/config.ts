130b08b6186aff74723ed1f96ae9006cd869f857
光 <2586300277@qq.com>
2023-03-18 15:24:01 +0800

    changerouter

---
+++ src/config/config.ts
@@ -0,0 +1,8 @@
+/* name path router title  */
+export const routers: Array<any> = [
+    'three——/three——/three/index——光伏',
+    'pipe——/pipe——/pipe/index——管道绘制',
+    'glsl——/glsl——/glsl/index——着色器',
+    'FBX——/FBX——/FBX/FBX——FBX加载',
+    'vector——/vector——/vector/vector——向量',
+]
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,36 +1,28 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Layout from '../Layout/Layout.vue'
+import { routers } from '../config/config'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '',
     component: Layout,
     redirect: '/glsl',
-    children: [
-      {
-        path: '/three',
-        component: () => import('../view/three/index.vue')
-      },
-      {
-        path: '/pipe',
-        component: () => import('../view/pipe/index.vue')
-      },
-      {
-        path: '/glsl',
-        component: () => import('../view/glsl/index.vue')
-      },
-      {
-        path: '/vector',
-        component: () => import('../view/vector/vector.vue')
-      },
-      {
-        path: '/FBX',
-        component: () => import('../view/FBX/FBX.vue')
-      }
-    ]
+    children: routers.map(i => getRoute(i))
   },
   
 ]
+
+function getRoute(str: string){
+  const [ name, path, routerStr, title ]: Array<string> =  str.split('——')
+  return {
+    name,
+    path,
+    component: () => import(/* @vite-ignore */"../view"+routerStr+".vue"),
+    meta: {
+      title
+    }
+  }
+}
 
 const router = createRouter({
   history: createWebHistory(),
