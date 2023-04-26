f41d18da0208f237b6e57485311537dc8cc3176d
zhangjingguang <2586300277@qq.com>
2023-04-26 16:18:15 +0800

    新发光

--- src/view/glsl/components/lightMoveShader.vue
+++ src/view/glsl/components/lightMoveShader.vue
@@ -35,56 +35,78 @@ function init(DOM:any) {
     onUnmounted(() => GUI.destroy())
 
     const uniforms:any = {
-        u_color: { value: new THREE.Color("#5588aa") },
-        u_tcolor: { value: new THREE.Color("#ff9800") },
-        u_r: { value: 0.25 },
-        u_length: { value: 80 },//扫过区域
-        u_max: { value: 800 }//扫过最大值
+        innerCircleWidth: {
+                        value: 0
+                    },
+        circleWidth: {
+            value: 300
+        },
+        diff: {
+            value: new THREE.Color(0.2, 0.2, 0.2)
+        },
+        color: {
+            value: new THREE.Color(0.0, 0.0, 1.0)
+        },
+        opacity: {
+            value: 0.3
+        },
+        center: {
+            value: new THREE.Vector3(0, 0, 0)
+        }
+    }
 
+    GUI.addColor({ color: 0xFFFFFF}, 'color').onChange((v:any) => uniforms.color.value = new THREE.Color(v)).name('修改发光颜色')
+    GUI.addColor({ color: 0xFFFFFF}, 'color').onChange((v:any) => uniforms.diff.value = new THREE.Color(v)).name('修改diffuse颜色')
+
+    function shaderAnimate() {
+        if ( uniforms.innerCircleWidth.value < 1000) {
+                uniforms.innerCircleWidth.value += 10
+        }
+        else {
+            uniforms.innerCircleWidth.value = 0
+        }
     }
 
     var geometry = new THREE.PlaneGeometry( 2000, 2000 );
     var material = new THREE.ShaderMaterial( {
         uniforms,
         vertexShader: `
-            varying vec3 vp;
-                void main(){
-                    vp = position; 
-                    gl_Position	= projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-                }
+            varying vec2 vUv;
+            varying vec3 v_position;
+            void main() {
+                vUv = uv;
+                v_position = position;
+                gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+            }
         `,
         fragmentShader: `
-               varying vec3 vp;
-                uniform vec3 u_color;
-                uniform vec3 u_tcolor;
-                uniform float u_r;
-                uniform float u_length;
-                uniform float u_max;
-                float getLeng(float x, float y){
-                    return  sqrt((x-0.0)*(x-0.0)+(y-0.0)*(y-0.0));
-                }
-                void main(){ 
-                    float uOpacity = 0.3; 
-                    vec3 vColor = u_color;
-                    float uLength = getLeng(vp.x,vp.y);
-                    if ( uLength <= u_r && uLength > u_r - u_length ) { 
-                        float op = sin( (u_r - uLength) / u_length ) * 0.6 + 0.3 ;
-                        uOpacity = op; 
-                        if( vp.y<0.0){
-                            vColor  = u_tcolor * 0.6; 
-                        }else{ 
-                            vColor = u_tcolor;
-                        };
-                    } 
-                    gl_FragColor = vec4(vColor,uOpacity);
-                }
+        varying vec2 vUv;
+        varying vec3 v_position;
+
+        uniform float innerCircleWidth;
+        uniform float circleWidth;
+        uniform float opacity;
+        uniform vec3 center;
+        
+        uniform vec3 color;
+        uniform vec3 diff;
+
+        void main() {
+            float dis = length(v_position - center);
+            if(dis < (innerCircleWidth + circleWidth) && dis > innerCircleWidth) {
+                float r = (dis - innerCircleWidth) / circleWidth;
+               
+                gl_FragColor = mix(vec4(diff, 0.1), vec4(color, opacity), r);
+            }else {
+                gl_FragColor = vec4(diff, 0.1);
+            }
+
+        }
             `
     })
 
     const mesh = new THREE.Mesh(geometry, material)
     scene.add(mesh)
-
-
     
     loadFBX('http://guangfu/aroundBuilding.FBX',  ((object3d:any) => {
 
@@ -105,10 +127,7 @@ function init(DOM:any) {
     function render(dalte:any) {
         fpsRender(() => {
             stats && stats.update()
-            uniforms.u_r.value += 3;
-            if (uniforms.u_r.value > uniforms.u_max.value) {
-                uniforms.u_r.value = 0;
-            }
+            shaderAnimate()
             renderer.render(scene,camera)
         })
         threeBox.value && requestAnimationFrame(render)
