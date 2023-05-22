7efa05969015499bd5188f97e0d1ec7fe938392c
zhangjingguang <2586300277@qq.com>
2023-05-22 10:58:42 +0800

    envMap

--- src/view/envMap/envMap.vue
+++ src/view/envMap/envMap.vue
@@ -10,7 +10,7 @@ import {
     setControls,getMaterials,objectDragHelper, 
     loadFBX ,setFpsClock, setOutLinePass , setStats,
     getWebGLMouse , clickIntersect,formatVertices, 
-    setPointsLineFaceGeometry,transFormControls,loadHDRTexture,loadEXRTexture
+    setPointsLineFaceGeometry,transFormControls,loadHDRTexture,loadEXRTexture,loadGltf
 } from '../three/threeApi'
 import { loadTiles, TilesUpadate, TilesBatchTable}  from '../three/tilesApi'
 import { createGUI } from '../three/GUI'
@@ -46,37 +46,59 @@ function initScene(DOM:any) {
 
     const sceneTexture = setSceneBackground(scene)
     const texture = new THREE.TextureLoader().load( 'https://img1.baidu.com/it/u=422078137,1307526884&fm=253&app=120&size=w931&n=0&f=JPEG&fmt=auto?sec=1684602000&t=0fca7d998a168e3581f32153d8eb55e7' )
+    texture.mapping = THREE.EquirectangularReflectionMapping;
     const hdrTexture = loadHDRTexture('/envMap/env.hdr',renderer)
+    hdrTexture.mapping = THREE.EquirectangularReflectionMapping;
     const exrTexture = loadEXRTexture('/envMap/env.exr',renderer)
-
+    
 
     // 环境贴图物体
-    const geometry = new THREE.BoxGeometry( 100, 100, 100 );
+    const geometry = new THREE.BoxGeometry( 1000, 1000, 1000 );
     const material = new THREE.MeshPhongMaterial( { color: 0xffffff, map: null } );
+    material.needsUpdate = true
     const cube = new THREE.Mesh( geometry, material );
+    cube.position.set(0,1500,0)
     scene.add( cube );
 
     const GUI = createGUI(THREE,scene,camera,controls)
+
     GUI.add({name:'种类'},'name', ['立体', '普通单张', 'hdr单张', 'exr单张']).onChange((v:any) => {
-       if(v=== '立体') {
+        env(v)
+    })
+
+    let materials:any = []
+    loadFBX('http://guangfu/zlcky/zlcky.FBX',  ((object3d:any) => {
+        scene.add(object3d)
+        materials = getMaterials(object3d)
+        env('普通单张')
+    }))
+
+    // loadGltf('http://guangfu/resource/test/树测试.glb', (o:any) => {
+    //     scene.add(o)
+    //     materials = getMaterials(o)
+    //     env('普通单张')
+    // })
+
+    function env(v:any) {
+        if(v=== '立体') {
           material.envMap = sceneTexture
-          material.needsUpdate = true
+          materials.forEach((i:any) => i.envMap = sceneTexture)
        }
        else if (v==='普通单张') {
             material.envMap = texture
-            texture.mapping = THREE.EquirectangularReflectionMapping;
-            material.needsUpdate = true
+            materials.forEach((i:any) => i.envMap = texture)
        }
        else if (v==='hdr单张'){
-            hdrTexture.mapping = THREE.EquirectangularReflectionMapping;
+            
             material.envMap =  hdrTexture
-            material.needsUpdate = true
+            materials.forEach((i:any) => i.envMap = hdrTexture)
        }
        else if (v==='exr单张'){
             material.envMap =  exrTexture
-            material.needsUpdate = true
+            materials.forEach((i:any) => i.envMap = exrTexture)
+            
        }
-    })
+    }
 
     const stats = setStats()
 
