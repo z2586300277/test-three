fa393d562519e39c93b35e332f7b020d98070aee
zhangjingguang <2586300277@qq.com>
2023-11-24 15:31:49 +0800

    bloom pass

---
+++ src/view/glsl/components/modelBloomShader.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="threeBox" ref="threeBox"></div>
+</template>
+
+<script lang="ts" setup>
+import * as THREE from 'three'
+import { ref , onMounted, onUnmounted, onActivated, onDeactivated} from 'vue';
+import { setControls, setStats , getMaterials} from '../../three/threeApi';
+import { createGUI } from '../three/GUI'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+import { BloomPass } from 'three/examples/jsm/postprocessing/BloomPass.js';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
+
+const threeBox = ref()
+onMounted(() => {
+    init(threeBox.value)
+})
+
+function init(DOM:any) {
+
+    const scene = new THREE.Scene()
+
+    const camera = new THREE.PerspectiveCamera(50,DOM.clientWidth / DOM.clientHeight, 0.1, 100000)
+
+    camera.position.set(0,0,10)
+    scene.add(camera);
+
+    const renderer = new THREE.WebGLRenderer({ antialias:true, alpha: true, logarithmicDepthBuffer: true  })
+    renderer.setSize(DOM.clientWidth, DOM.clientHeight)
+    renderer.setPixelRatio( window.devicePixelRatio * 2)
+    renderer.setClearColor( 0x000000 )
+    DOM.appendChild(renderer.domElement)
+
+    const controls =  setControls(camera, renderer)
+    const axes = new THREE.AxesHelper(5500)
+    scene.add(axes)
+
+    const light = new THREE.AmbientLight(0xffffff, 1)
+    scene.add(light)
+
+    const box = new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshStandardMaterial({color:'yellow'}))
+    scene.add(box)
+
+    const bloomPass = new BloomPass( 1.25 );
+
+    const composer = new EffectComposer(renderer);
+    composer.addPass(new RenderPass(scene, camera));
+    composer.addPass(bloomPass);
+    composer.addPass(new OutputPass());
+
+    render()
+    function render() {
+        composer.render();
+        threeBox.value &&  requestAnimationFrame(render)
+    }
+
+}
+</script>
+
+<style lang="less" scoped>
+.threeBox {
+    width: 100%;
+    height: 100%;
+}
+.btn {
+    position: absolute;
+    left: 50vw;
+    z-index: 100;
+}
+</style>
