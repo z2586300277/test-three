79d997225339a5fce6951d140331176b0108f8a0
zhangjingguang <2586300277@qq.com>
2024-02-28 11:24:32 +0800

    gis

--- src/view/cesium/cesium.vue
+++ src/view/cesium/cesium.vue
@@ -4,14 +4,11 @@
 
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue'
-import * as Cesium from 'cesium'
-import "cesium/Build/Cesium/Widgets/widgets.css"
-Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjOWM5N2FlYy0wNTIyLTRhZDAtYTM2Yi04ZGEzYzMzNTRjYmQiLCJpZCI6NTcwNzEsImlhdCI6MTYyMjAxMjY3MH0.IRCEryIGGPZeCGqJ8lpMESuZO9DTIlLnF-WS-w9YYzc"
+import { GisSceneEditor } from '@gdmp/gis-scene-editor'
 onMounted(() => {
-    const viewer = new Cesium.Viewer(document.getElementById('cesiumContainer') as HTMLElement, {
-        infoBox: false,
-    })
+    new GisSceneEditor(document.getElementById('cesiumContainer'))
 })
+
 </script>
 
 <style scoped>
